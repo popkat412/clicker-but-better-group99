07e6f922ee27275d7313a4cf9e3b77d5c525011f
wangy <wangyunze412@gmail.com>
2020-08-22 11:12:50 +0800

    Added timer and instruction label to clicker

--- Clicker but Better/ClickerViewController.swift
+++ Clicker but Better/ClickerViewController.swift
@@ -11,17 +11,31 @@ import UIKit
 class ClickerViewController: UIViewController {
 
     @IBOutlet weak var clickerLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
     var count = 0
+    var secondsPassed = 0
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.secondsPassed += 1
+            print("Updating timer: \(self.secondsPassed)")
+        }
     }
 
     @IBAction func screenTapped(_ sender: Any) {
-
         count += 1
         clickerLabel.text = "\(count)"
+        
+        if count >= 20 {
+            timer.invalidate()
+            clickerLabel.text = "You took \(secondsPassed)s"
+            instructionLabel.isHidden = true
+        }
     }
     
 }
