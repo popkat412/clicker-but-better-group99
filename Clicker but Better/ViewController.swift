e6a9761b873443990051aeb3425052aa532700a8
wangy <wangyunze412@gmail.com>
2020-08-22 10:21:51 +0800

    Initial Commit

---
+++ Clicker but Better/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker but Better
+//
+//  Created by Wang Yunze on 22/8/20.
+//  Copyright © 2020 yunze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
